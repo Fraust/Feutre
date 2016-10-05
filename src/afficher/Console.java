ceb3b10181496fcfcd6529f321b7ce2dfb1985af
sostac <sostac@pc107-06.bts.lan>
2016-10-05 10:41:31 +0200

    Feutre

---
+++ src/afficher/Console.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package afficher;
+
+/**
+ *
+ * @author Sostac
+ */
+public class Console implements AffichageTxt{
+
+    @Override
+    public void setAffichage(String texte) {
+        System.out.println(texte);
+    }
+
+    @Override
+    public void setAffichage(char texte) {
+        System.out.print(texte);
+    }
+    
+    
+            
+}
